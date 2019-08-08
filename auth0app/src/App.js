8d3865221c88fc05763b3cf490ba3b140fb143c4
CarloC24 <carlo.clamucha@gmail.com>
2019-08-07 23:54:48 -0500

    finally got it to work

--- auth0app/src/App.js
+++ auth0app/src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import App1 from "./app1";
+import myContext from "./import";
+// console.log(myContext);
 
-export const MyContext = React.createContext();
+export let poop = "poop";
 function ContextProvider({ children }) {
+  const [count, setCount] = useState(0);
   return (
-    <MyContext.Provider value={{ count: 0, name: "Carlo" }}>
+    <myContext.Provider value={{ count, name: "Carlo :poop:", setCount }}>
       {children}
-    </MyContext.Provider>
+    </myContext.Provider>
+  );
+}
+function AnotherComponent() {
+  return (
+    <div>
+      <p>This is a another component</p>
+      <myContext.Consumer>
+        {context => <p>{context.name}</p>}
+      </myContext.Consumer>
+    </div>
   );
 }
 
@@ -16,9 +29,6 @@ function App() {
   return (
     <div className="App">
       <ContextProvider>
-        <MyContext.Consumer>
-          {context => <p>rendering from app {context.count}</p>}
-        </MyContext.Consumer>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -32,6 +42,7 @@ function App() {
           >
             Learn React
           </a>
+          {/* <AnotherComponent /> */}
           <App1 />
         </header>
       </ContextProvider>
--- auth0app/src/app1.js
+++ auth0app/src/app1.js
@@ -1,15 +1,13 @@
 import React from "react";
 import App2 from "./app2";
-import { MyContext } from "./App";
-
+import MyContext from "./import";
 export default function app1() {
   return (
     <div>
-      <p>App 1</p>
-      {/* <MyContext.Consumer>
-        {" "}
-        {context => <p>{context.value}</p>}
-      </MyContext.Consumer> */}
+      <p>App 1 access the data</p>
+      <MyContext.Consumer>
+        {context => <p>{context.name} this is app 1</p>}
+      </MyContext.Consumer>
       <App2 />
     </div>
   );
