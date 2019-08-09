ebadee68049973c218b7a453d6ca08207484b9b1
CarloC24 <carlo.clamucha@gmail.com>
2019-08-08 22:32:19 -0500

    added stuff

--- auth0app/src/app1.js
+++ auth0app/src/app1.js
@@ -1,10 +1,12 @@
 import React from "react";
 import App2 from "./app2";
 import MyContext from "./import";
+import { Notdefined } from "./import";
 export default function app1() {
   return (
     <div>
       <p>App 1 access the data</p>
+      <Notdefined />
       <MyContext.Consumer>
         {context => <p>{context.name} this is app 1</p>}
       </MyContext.Consumer>
