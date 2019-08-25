d87dd5499b71c5cf030d3130757be8800fd5cf4e
CarloC24 <carlo.clamucha@gmail.com>
2019-08-25 09:30:49 -0500

    renewed folder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import App1 from "./app1";
+import myContext from "./import";
+import Img from "./img";
+import axios from "axios";
+
+function ContextProvider({ children }) {
+  const [count, setCount] = useState(0);
+  return (
+    <myContext.Provider value={{ count, name: "Carlo :poop:", setCount }}>
+      {children}
+    </myContext.Provider>
+  );
+}
+
+function App() {
+  useEffect(() => {
+    async function postData() {
+      const res = await axios.get("http://localhost:5000");
+      console.log(res);
+    }
+    postData();
+  }, []);
+  return (
+    <div className="App">
+      <ContextProvider>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {/* <AnotherComponent /> */}
+          {/* <App1 /> */}
+          <Img />
+        </header>
+      </ContextProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/img.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function() {
+  const [myImages, setMyImages] = useState([]);
+  async function uploadImage(e) {
+    const files = e.target.files;
+    let formData = new FormData();
+    formData.append("file", files[0]);
+    formData.append("data", "carlo");
+    const res = await axios.post("http://localhost:5000/formdata", {
+      data: formData
+    });
+    console.log(res);
+  }
+  return (
+    <div>
+      <input type="file" onChange={uploadImage} />
+      {myImages && myImages.map(item => <img src={item} alt="wow" />)}
+    </div>
+  );
+}
