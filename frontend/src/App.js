908f2c0d32f5a053b64513b0df7e7a17c25a2830
CarloC24 <carlo.clamucha@gmail.com>
2019-10-21 21:58:57 -0700

    finished playing with formik

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,34 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import App1 from "./app1";
 import { MyContextProvider } from "./import";
+import { Formik, Field } from "formik";
 
 function App() {
   return (
     <MyContextProvider>
       <div className="App">
-        <App1></App1>
+        {/* <App1 /> */}
+        <Formik
+          initialValues={{ name: "carlo" }}
+          onSubmit={(values, actions) => {
+            console.log(values, actions);
+          }}
+          render={props => (
+            <form onSubmit={props.handleSubmit}>
+              <Field
+                type="text"
+                name="name"
+                onBlur={props.handleBlur}
+                value={props.values.name}
+                onSubmit={props.handleChange}
+              />
+              <button type="submit" disabled={props.isSubmitting}>
+                Submit
+              </button>
+            </form>
+          )}
+        />
+        <p>Cool</p>
       </div>
     </MyContextProvider>
   );
